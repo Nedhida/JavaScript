fd5b26db0b19fff77d173d694694617078c89d7a
Nedhida <pinedoutshadeyouth@gmail.com>
2022-03-18 15:08:22 +0900

    [Fin]js-11

--- Re_js/Tabbar-Lesson/js/script.js
+++ Re_js/Tabbar-Lesson/js/script.js
@@ -1,8 +1,17 @@
-$(function () {
-  $('.menu-trigger').on('click', function (event) {
-    // 指定されているclass（active）をトグル処理するメソッド　トグル処理：該当classがある場合は削除、無い場合は追加する処理
-    $(this).toggleClass('active');
-    $('#sp-menu').fadeToggle();
-    event.preventDefault();
-  });
+// TAB1のコンテンツ以外を非表示
+$('#tab-contents .tab[id != "tab1"]').hide();
+// ↑　$('要素[属性]') 特定の属性を持つ要素や属性値を絞り込む。!= "値"　属性値が特定の値に等しくない要素だけを取得
+
+// それぞれのタブがクリックされた時のイベント
+$('#tab-menu a').on('click', function (event) {
+  // タブ内のコンテンツを一旦非表示
+  $("#tab-contents .tab").hide();
+  // .activeクラスを削除
+  $("#tab-menu .active").removeClass("active");
+  // クリックされたタブに.activeを付与
+  $(this).addClass("active");
+  // クリックされたタブにhref属性を取得
+  $($(this).attr("href")).show();
+  //↑　.attr() html要素の属性を取得or設定できる
+  event.preventDefault();
 });
