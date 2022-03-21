335542da3e685249514426ca04e67a2d6c17a960
Nedhida <pinedoutshadeyouth@gmail.com>
2022-03-22 08:40:18 +0900

    [Fin]js-12

--- Re_js/ajax/script.js
+++ Re_js/ajax/script.js
@@ -1,17 +1,31 @@
-// TAB1のコンテンツ以外を非表示
-$('#tab-contents .tab[id != "tab1"]').hide();
-// ↑　$('要素[属性]') 特定の属性を持つ要素や属性値を絞り込む。!= "値"　属性値が特定の値に等しくない要素だけを取得
+const API_KEY = "29d202762f2ab7590fb2250559f29e4c";
 
-// それぞれのタブがクリックされた時のイベント
-$('#tab-menu a').on('click', function (event) {
-  // タブ内のコンテンツを一旦非表示
-  $("#tab-contents .tab").hide();
-  // .activeクラスを削除
-  $("#tab-menu .active").removeClass("active");
-  // クリックされたタブに.activeを付与
-  $(this).addClass("active");
-  // クリックされたタブにhref属性を取得
-  $($(this).attr("href")).show();
-  //↑　.attr() html要素の属性を取得or設定できる
-  event.preventDefault();
+$(function () {
+  $('#btn').on('click', function () {
+    // 入力された都市名でWebAPIに天気情報をリクエスト
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType: 'jsonp',
+    }).done(function (data) {
+      //通信成功
+      // 位置
+      $('#place').text(data.name);
+      // 最高気温
+      $('#temp_max').text(data.main.temp_max);
+      // 最低気温
+      $('#temp_min').text(data.main.temp_min);
+      //湿度
+      $('#humidity').text(data.main.humidity);
+      //風速
+      $('#speed').text(data.wind.speed);
+      // 天気
+      $('#weather').text(data.weather[0].main);
+      // 天気アイコン
+      $('img').attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt", data.weather[0].main);
+    }).fail(function (data) {
+      //通信失敗
+      alert('通信に失敗しました。');
+    });
+  });
 });
